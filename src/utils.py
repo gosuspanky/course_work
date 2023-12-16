76a3cca0ec233d16d41a37e609f6444918acc2c4
Artur Gaetov <agaetov@gmail.com>
2023-12-16 21:17:28 +0300

    fix utils description

--- src/utils.py
+++ src/utils.py
@@ -13,3 +13,29 @@ def get_data():
 
     data = json.loads(json_data)
     return data
+
+
+def split_text_numbers(bill):
+    text = ""
+    numbers = ""
+    res = []
+    for i in bill:
+        if i.isdigit():
+            numbers += i
+        else:
+            text += i
+    res.append(text.strip())
+    res.append(numbers)
+    return res
+
+
+def encrypt_bill_num(bill_num):
+    """
+    Функция, определяющая счет это или карта
+    :param bill_num: номер карты или счета
+    :return: возвращает только цифры карты или счета
+    """
+    if len(bill_num) == 20:
+        return f'**{bill_num[-4:]}'
+    elif len(bill_num) == 16:
+        return f'{bill_num[:4]} {bill_num[4:6]}** **** {bill_num[-4:]}'
