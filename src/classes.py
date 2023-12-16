fecb9d7dfc7070f32e5af46b177058ea0d30b2f1
Artur Gaetov <agaetov@gmail.com>
2023-12-16 21:16:40 +0300

    added Class Operation

---
+++ src/classes.py
@@ -0,0 +1,36 @@
+from src.utils import split_text_numbers, encrypt_bill_num
+import datetime
+
+
+class Operation:
+    def __init__(self, operation_data):
+        self.operation_data = operation_data
+
+    def new_time(self):
+        """
+        Метод переделывает формат даты
+        :return: Возвращает нужную дату для вывода
+        """
+        operation_time = self.operation_data['date']
+        change_time = operation_time.split('T')
+        operation_time_new = " ".join(change_time)
+        date_time_obj = datetime.datetime.strptime(operation_time_new, '%Y-%m-%d %H:%M:%S.%f')
+        new_time = date_time_obj.strftime('%d.%m.%Y')
+        return new_time
+
+
+    def encrypt_bill(self):
+        """
+        Метод, использующий функцию кодирования номеров карт или счетов
+        :return: возвращает кортеж строк счетов или карт операции
+        """
+        sender = split_text_numbers(self.operation_data['from'])
+        receiver = split_text_numbers(self.operation_data['to'])
+
+        sender[1] = encrypt_bill_num(sender[1])
+        receiver[1] = encrypt_bill_num(receiver[1])
+
+        sender_new = " ".join(sender)
+        receiver_new = " ".join(receiver)
+
+        return sender_new, receiver_new
